ad0c3d2006baf1f4a764629dbdb906a2fb5a7a00
linodai <lino.td8955@gmail.com>
2024-06-08 09:46:58 +0900

    mongodb接続

--- routes/notes.js
+++ routes/notes.js
@@ -1,32 +1,22 @@
 var express         = require('express');
 var router          = express.Router();
 
-// レスポンスのデータ（ノート全体）
-const responseObjectDataAll = {
-    textObejct1 : {
-        id: 1,
-        title: 'ノート１のタイトルです',
-        subTitle: 'ノート１のサブタイトルです',
-        bodyTitle: 'ノート１の本文です'
-    },
-    textObejct2 : {
-        id: 2,
-        title: 'ノート2のタイトルです',
-        subTitle: 'ノート2のサブタイトルです',
-        bodyTitle: 'ノート2の本文です'
-    }
-};
+// 接続情報を設定
+const { MongoClient }   = require("mongodb");
+const uri               = "***************";
+const client            = new MongoClient(uri);
 
-/**
- * メモ全権取得するAPI
- * @returns {Object[]} data
- * @returns {number} data.id - ID
- * @returns {string} data.title - タイトル
- * @returns {string} data.text - 内容
- */
-router.get('/', function (req, res, next) {
-    // 全件取得して返す
-    res.json(responseObjectDataAll);
+router.get('/', async (req, res) => {
+
+    // データベース、コレクションを指定
+const database          = client.db('notes');
+const notes             = database.collection('notes');
+
+// idが1のドキュメントを取得
+const query             = { id: 2 };
+const note              = await notes.findOne(query);
+
+res.json(note.title);
 })
 
 module.exports = router;
